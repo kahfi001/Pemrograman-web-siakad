a26a45e401d437843599dd12ac29282a5a156fd3
muhammadkahfi <72506314+kahfi001@users.noreply.github.com>
2021-10-02 00:44:48 +0700

    mengubah class container menjadi container-login

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,20 +1,18 @@
+<div class="container-login  rounded">
+    <!-- Content here -->
+    <img src="<?= BASEURL; ?>/img/Unesa.png" class="rounded mx-auto d-block" style="width: 100px;">
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">E-mail/user</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+    </form>
+    <a href="<?= BASEURL; ?>/dashboard">
+        <button type="submit" class="btn " style="background-color: #FFC007">Submit</button>
+    </a>
 
-<body>
-    <div class="container rounded">
-        <!-- Content here -->
-        <img src="<?= BASEURL; ?>/img/Unesa.png" class="rounded mx-auto d-block" style="width: 100px;">
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">E-mail/user</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <button type="submit" class="btn " style="background-color: #FFC007">Submit</button>
-        </form>
-
-    </div>
-
-</body>
+</div>
