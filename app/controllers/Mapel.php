7ebe558261244deb2ac3475a37ced032aadbb200
bagas12dwi <bag12dwi.s@gmail.com>
2021-10-10 18:58:18 +0700

    sudah bisa connect database

--- app/controllers/Mapel.php
+++ app/controllers/Mapel.php
@@ -5,9 +5,10 @@ class Mapel extends Controller{
     public function index()
     {
         $data['tittle'] = 'Mata Pelajaran';
+        $data['mapel'] = $this->model('mapel_model')->getAllMapel();
         $this->view('template/header', $data);
         $this->view('template/navbar');
-        $this->view('mata-pelajaran/mapel');
+        $this->view('mata-pelajaran/mapel', $data);
         $this->view('template/footer');
     }
 }
