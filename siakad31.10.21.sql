2f827e5a299ebcbde3159c6f3be036aa1f876468
muhammadkahfi <72506314+kahfi001@users.noreply.github.com>
2021-10-31 02:48:31 +0700

    Add files via upload

---
+++ siakad31.10.21.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2021 at 09:47 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siakad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guru`
+--
+
+CREATE TABLE `guru` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_hp` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guru`
+--
+
+INSERT INTO `guru` (`id`, `nama`, `nip`, `alamat`, `email`, `no_hp`) VALUES
+(1, 'Fiersa besari', '20013434534', 'Jakarta', 'fiersa@gmail.com', '851235567112'),
+(2, 'Tiara Andini', '20013434432', 'Bandung', 'tiara@gmail.com', '851235567332'),
+(6, 'sasuke', '45213', 'jalan imam bonjol gang mimbar no.72 singaraja, bali', 'sadfeaf@afd', '081246504639'),
+(7, 'naruto', '5123', 'jalan imam bonjol gang mimbar no.72 singaraja, bali', 'naruto@gmail.com', '081246504639'),
+(8, 'anang', '231', 'bogor', 'anang@gmail.com', '089999999');
+
+--
+-- Triggers `guru`
+--
+DELIMITER $$
+CREATE TRIGGER `add_user_guru` AFTER INSERT ON `guru` FOR EACH ROW INSERT INTO users (username, nama, password, level)
+
+SELECT nip, nama, nip, 'guru' FROM guru ORDER BY id DESC LIMIT 1
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `delete_user_guru` BEFORE DELETE ON `guru` FOR EACH ROW DELETE FROM users WHERE users.username = OLD.nip
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL,
+  `kelas` varchar(100) NOT NULL,
+  `wali_kelas` varchar(100) NOT NULL,
+  `jumlah_murid` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id`, `kelas`, `wali_kelas`, `jumlah_murid`) VALUES
+(1, '1A', 'Endank Nurhayati', '30'),
+(2, '2A', 'Juhairi Wong', '24'),
+(3, '3A', 'Nur Romanoff', '32'),
+(4, '5', 'Budiwati', '32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapel`
+--
+
+CREATE TABLE `mapel` (
+  `id` int(11) NOT NULL,
+  `nama_mapel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mapel`
+--
+
+INSERT INTO `mapel` (`id`, `nama_mapel`) VALUES
+(1, 'Matematika'),
+(2, 'Bahasa Indonesia'),
+(3, 'Bahasa Inggris'),
+(4, 'PPKn'),
+(5, 'olahraga'),
+(6, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nis` varchar(100) NOT NULL,
+  `kelas` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_hp` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id`, `nama`, `nis`, `kelas`, `alamat`, `no_hp`) VALUES
+(1, 'andre', '54321', '4', 'kenjeran', '0897777'),
+(2, 'kholiqul', '54213', '2', 'ponorogo', '0986524567'),
+(3, 'bagas', '4567', '3', 'rungkut', '145656');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `nama`, `password`, `level`) VALUES
+(1, '20013434534', 'Fiersa besari', '20013434534', 'guru'),
+(2, '20013434432', 'Tiara Andini', '20013434432', 'guru'),
+(4, 'admin', 'admin1', 'admin123', 'admin'),
+(7, '45213', 'sasuke', '45213', 'guru'),
+(8, '5123', 'naruto', '5123', 'guru'),
+(9, '231', 'anang', '231', 'guru');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mapel`
+--
+ALTER TABLE `mapel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `guru`
+--
+ALTER TABLE `guru`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `mapel`
+--
+ALTER TABLE `mapel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
